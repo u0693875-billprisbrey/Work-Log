e0fc8de47abf11b5f2ff3f5de317bc13bdd6350c
Bill Prisbrey <u0693875@umail.utah.edu>
2026-03-30 16:09:48 -0600

    Weekly log update and fitness tracking update

---
+++ Email Body/Email for week starting 03.23.2026.R
@@ -0,0 +1,88 @@
+# Email for week starting 03.23.2026  
+
+# Load and Prep
+
+library(kableExtra)
+library(blastula)
+library(glue)
+
+source(here::here("Prep", "Notepad Email Prep.R"))
+
+
+##################
+## Aggregations ##
+##################
+
+recentCategory <- aggregate(diff ~ CATEGORY, data = mostRecent, sum, na.rm=TRUE)
+dailyActivity <- aggregate(diff ~ DATE, data = mostRecent, sum, na.rm = TRUE)
+
+category_by_day <- xtabs(diff ~ DATE + CATEGORY, 
+                         data = aggregate(diff ~ DATE + CATEGORY, data = mostRecent, sum, na.rm = TRUE))
+
+# category_by_day |> addmargins() # useful for double-checking
+
+dailyDisplay <- function() {
+  # For display
+  
+  category_by_day |>
+    addmargins() |>
+    as.data.frame.matrix() |>
+    kable() |>
+    kable_styling(bootstrap_options = c("striped", "hover"))
+  
+}  
+
+summaryAgg <- recentCategory[!recentCategory$CATEGORY %in% c("Break", "Fitness Challenge"),]
+
+checkTime <- function(){
+  sum(summaryAgg$diff)
+}
+
+# mostRecent[mostRecent$CATEGORY == "Job Mngt",]
+# mostRecent[mostRecent$CATEGORY == "Study Causal Inference",]
+
+explanations <- c("Job Mngt" = "Daily planning; weekly log; UAIR meetings", 
+                  "Predict Math Success" = "Phase 2 sneak preview graphic; Re-training and optimizing model without caret; deep dive report",
+                  "PI Turnover" = "",
+                  "Gray DI" = "Training series; Q&A during office hrs; developing analysis priorities",
+                  "Prof Dev" = "",
+                  "Sick" = "Appts for child"
+)  
+
+summaryAgg <- merge(summaryAgg, as.data.frame(explanations), by.x = "CATEGORY", by.y = "row.names", all.x = TRUE )
+
+####################
+## EMAIL SECTIONS ##
+####################
+
+assignments <- 
+  "
+* Add two appendices: Sneak Preview and comparing predictions without test scores 
+* Write up a brief summary of student course feedback work  
+* Review conferences and possible submission titles for papers, prioritizing student course feedback  
+* Review Roles & Responsibilities and HR guidelines and put together a plan for title change to Senior/Lead/Principal Data Scientist
+"
+
+agendaItems <- 
+  "
+  * Possible conferences  
+  * Student course feedback summary
+  * Completions early findings
+"
+
+nextSteps <- 
+  "
+* Become competent in new training workflow (that doesn't use 'caret') DONE
+* Update select models using 'tidymodels' and 'xgboost' (instead of 'caret') IN PROGRESS
+* Update Phase 1 report per feedback  DONE
+* Complete Phase 2 (Course Guide and Estimate Academic Impact)  
+* PI Turnover:  Review snapshot query from HR and write summary before pausing project      
+* Develop Gray DI analytical approach based on internal objectives and data availability IN PROGRESS   
+"
+
+################
+## SEND EMAIL ##
+################
+
+# source(here::here("Send Email", "Send Email (To Prisbrey Only).R")) 
+source(here::here("Send Email", "Send Email (To Luis).R"))
