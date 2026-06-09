c8a3d13f0609a4a65d48ef1dd7ac04b8bef21aec
Bill Prisbrey <u0693875@umail.utah.edu>
2026-06-09 10:21:40 -0600

    Three weeks of logs (last two weeks of May and first week of June.)

---
+++ Email Body/Email for week starting 05.18.2026.R
@@ -0,0 +1,81 @@
+# Email for week starting 05.18.2026  
+
+# Load and Prep
+
+library(kableExtra)
+library(blastula)
+library(glue)
+
+source(here::here("Prep", "Notepad Email Prep.R"))
+
+##################
+## Aggregations ##
+##################
+
+recentCategory <- aggregate(diff ~ CATEGORY, data = mostRecent, sum, na.rm=TRUE)
+dailyActivity <- aggregate(diff ~ DATE, data = mostRecent, sum, na.rm = TRUE)
+
+category_by_day <- xtabs(diff ~ DATE + CATEGORY, 
+                         data = aggregate(diff ~ DATE + CATEGORY, data = mostRecent, sum, na.rm = TRUE))
+
+# category_by_day |> addmargins() # useful for double-checking
+
+dailyDisplay <- function() {
+  # For display
+  
+  category_by_day |>
+    addmargins() |>
+    as.data.frame.matrix() |>
+    kable() |>
+    kable_styling(bootstrap_options = c("striped", "hover"))
+  
+}  
+
+summaryAgg <- recentCategory[!recentCategory$CATEGORY %in% c("Break", "Fitness Challenge"),]
+
+checkTime <- function(){
+  sum(summaryAgg$diff)
+}
+
+# mostRecent[mostRecent$CATEGORY == "Job Mngt",]
+# mostRecent[mostRecent$CATEGORY == "Study Causal Inference",]
+
+explanations <- c("Job Mngt" = "UAIR Meetings; weekly logs; Jira",
+                  # "Holiday" = "Memorial Day",
+                  # "Sick" = "Dr Appt",
+                  # "Travel" = "Internal networking",
+                  "Forecast Completion Rates" = "Diagnostics, plots, double-checking, and de-bugging" 
+)  
+
+summaryAgg <- merge(summaryAgg, as.data.frame(explanations), by.x = "CATEGORY", by.y = "row.names", all.x = TRUE )
+
+####################
+## EMAIL SECTIONS ##
+####################
+
+assignments <- 
+  "
+* Review conferences and possible submission titles for papers  
+* Develop forecast from FTF Demo data, disregarding other data sources IN PROGRESS
+"
+
+agendaItems <- 
+  "
+* See next email
+"
+
+nextSteps <- 
+  "
+* Develop baseline forecast using historical proportions (stratifying by gender and declared major) ABANDONED
+* Develop forecast using extreme gradient boosting IN PROGRESS
+* Develop forecast using survival analysis 
+* Combine all forecasts as an ensemble forecast
+"
+
+################
+## SEND EMAIL ##
+################
+
+# source(here::here("Send Email", "Send Email (To Prisbrey Only).R")) 
+source(here::here("Send Email", "Send Email (To Luis).R"))
+
